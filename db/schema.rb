cb017d52054c8464a9464ef23f603399b39c8f06
Harry Nguyen <dnncntt@gmail.com>
2016-02-05 20:37:48 +0700

    adding comment basic layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204154015) do
+ActiveRecord::Schema.define(version: 20160205132748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,15 @@ ActiveRecord::Schema.define(version: 20160204154015) do
     t.string   "feature"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  add_foreign_key "comments", "articles"
 end
