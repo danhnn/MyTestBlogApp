6e9994a6b3c0b5e15dd10c872867f70f65ebf835
Harry Nguyen <dnncntt@gmail.com>
2016-02-05 14:30:39 +0700

    add new font

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
 
-  
-  get '/about', to: "articles#about" 
+
+  get '/about', to: "articles#about"
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
